861fc9fcb00d4344367625ce8a5cebd880ae35e5
PaulinaPaw <paulinapatrycja.fior@gmail.com>
2022-01-11 21:49:12 +0100

    Add content, styling and js for data block

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// clock consts
 const hour = document.getElementById('clock-hour');
 const minutes = document.getElementById('clock-minutes');
 const seconds = document.getElementById('clock-seconds');
@@ -18,3 +19,70 @@ const clock = () => {
 };
 
 setInterval(clock, 1000); //launch clock function each 1s
+
+//text block consts
+const textHours = document.getElementById('text-hour');
+const textMinutes = document.getElementById('text-minutes');
+const textSeconds = document.getElementById('text-seconds');
+const textZone = document.getElementById('text-zone');
+const dateDay = document.getElementById('text-day');
+const dateMonth = document.getElementById('text-month');
+const dateYear = document.getElementById('text-year');
+
+// Text block changes
+const textDate = () => {
+  let date = new Date();
+
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let zone = date.getTimezoneOffset() / -60;
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
+  if (hh < 10) {
+    textHours.innerText = `0${hh}:`;
+  } else {
+    textHours.innerText = `${hh}:`;
+  }
+
+  if (mm < 10) {
+    textMinutes.innerText = `0${mm}:`;
+  } else {
+    textMinutes.innerText = `${mm}:`;
+  }
+
+  if (ss < 10) {
+    textSeconds.innerText = `0${ss}`;
+  } else {
+    textSeconds.innerText = `${ss}`;
+  }
+
+  if (Math.sign(zone) === -1) {
+    textZone.innerText = `UTC ${zone}`;
+  } else {
+    textZone.innerText = `UTC +${zone}`;
+  }
+
+  dateDay.innerText = day;
+
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Nov',
+    'Dec',
+  ];
+
+  dateMonth.innerText = `${months[month]}`;
+  dateYear.innerText = year;
+};
+
+setInterval(textDate, 1000); //launch clock function each 1s
